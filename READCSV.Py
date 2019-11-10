674363f820bc6fcd602d10c905a58529746ece40
kaur0526 <54955041+kaur0526@users.noreply.github.com>
2019-11-10 15:23:19 -0500

    Update READCSV.Py

--- READCSV.Py
+++ READCSV.Py
@@ -1,1 +1,21 @@
+if __name__ == '__main__':
+
+    # writing boy names to csv
+    outputFile = open('2000_BoysNames.csv', 'w')
+    print >> outputFile, '"FirstName",Count'
+    with open('2000_BoysNames.txt') as fp:
+        for line in fp:
+            line = line.strip('\n')
+            firstName, count = line.split(' ', 1)
+            print >> outputFile, '"' + firstName + '"' + ',' + count
+
+    # writing girl names to csv
+    outputFile = open('2000_GirlsNames.csv', 'w')
+    print >> outputFile, '"FirstName",Count'
+    with open('2000_GirlsNames.txt') as fp:
+        for line in fp:
+            line = line.strip('\n')
+            firstName, count = line.split(' ', 1)
+            print >> outputFile, '"' + firstName + '"' + ',' + count 
+
 
